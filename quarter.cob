0de10e0c73071bc735903844784979c0c014686e
Ana Mercedes Gauna <amgauna@gmail.com>
2017-04-15 00:49:20 -0300

    Update quarter.cob

--- quarter.cob
+++ quarter.cob
@@ -46,4 +46,62 @@ FD Arquivo-Vendas
    05 Cota-Registro-Vendas         PIC S9(5)V99.
    05 Filler                       PIC X(40).
    
-   
+FD Relat-Trimestral
+   Label Records are Omitted
+   Record Contains 132 Characters
+   .
+01 Linha-Relat-Trimestral          PIC X(132).
+      
+Working-Storage-Section.
+      
+01 Chaves-Totais.
+   05 Fim-Arquivo-Vendas           PIC X.
+   05 Total-Trimestre              PIC S9(5)V99 COMP-3.
+01 Linha-Relatorio-Trabalho.
+   05 Nome-Trabalho                PIC X(15).
+   05 Filler                       PIC X(5) VALUE Spaces.
+   05 Total-Trabalho               PIC $$$.$$$,99.
+   05 Filler                       PIC X(5) VALUE Spaces.
+   05 Cota-Trabalho                PIC $$$.$$$,99.
+   05 Filler                       PIC X(87) Value Spaces.
+      
+Procedure Division.
+      
+010 - Parag-Executivo.
+      Perform 020-Inicializar-E-Preparar
+      Perform 040-Imprimir-Linhas-Relatorio
+         Ultil Fim-Arquivo-Vendas = "T"
+      Perform 050-Termino-E-encerramento
+      Stop RUN
+      .
+      
+020 - Inicializar-E-Preparar.
+      Open Input Arquivo-Vendas
+           Output Relat-Trimestral
+      Move "F" to Fim-Arquivo-Vendas
+      Perform 030-Ler-Arquivo-Vendas
+      .
+      
+030 - Ler-Arquivo-Vendas.
+      Read Arquivo-Vendas
+           At END
+                 Move "T" to Fim-Arquivo-Vendas
+      .
+      
+040 - Imprimir-Linhas-Relatório.
+      Move Nome-Registro-Vendas TO Nome-Trabalho
+      Compute Total-Trimestre = Vendas-Mes-1-Registro-Vendas
+                              + Vendas-Mes-2-Registro-Vendas
+                              + Vendas-Mes-3-Registro-Vendas
+      Move Total-Trimestre TO Total-Trabalho
+      Move Cota-Registro-Vendas TO Cota-Trabalho
+      Write Linha-Relat-Trimestral
+            From Linha-Relatorio-Trabalho
+      Perform 030-Ler-Arquivo-Vendas
+      .
+      
+050 - Termino-E-Encerramento.
+      CLOSE Arquivo-Vendas
+            Relat-Trimestral
+      
+      
